35785b92fb5984cf94c6087fb8d868a3512df4a8
iknowmagic <ilan@iknowmagic.net>
2020-09-13 14:01:11 -0700

    yarn updates

--- vue.config.js
+++ vue.config.js
@@ -8,7 +8,7 @@ module.exports = {
      * https://blog.scottlogic.com/2017/11/01/webpack-source-map-options-quick-guide.html
      * https://cli.vuejs.org/guide/mode-and-env.html#modes
      */
-    // devtool: process.env.NODE_ENV === 'production' ? 'none' : 'eval-source-map'
+    devtool: process.env.NODE_ENV === 'production' ? 'none' : 'eval-source-map',
 
     output:
       process.env.NODE_ENV === 'production'
