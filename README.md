5c0fe9812c90d64171719226267ba5aaf7d78e1a
iknowmagic <ilan@iknowmagic.net>
2020-09-13 20:57:47 -0700

    sentry, snyk

--- README.md
+++ README.md
@@ -1,24 +1,31 @@
+[![Known Vulnerabilities](https://snyk.io/test/github/iKnowMagic/rock-paper-scissors/badge.svg?targetFile=package.json)](https://snyk.io/test/github/iKnowMagic/rock-paper-scissors?targetFile=package.json)
+
 # project
 
 ## Project setup
+
 ```
 yarn install
 ```
 
 ### Compiles and hot-reloads for development
+
 ```
 yarn serve
 ```
 
 ### Compiles and minifies for production
+
 ```
 yarn build
 ```
 
 ### Lints and fixes files
+
 ```
 yarn lint
 ```
 
 ### Customize configuration
+
 See [Configuration Reference](https://cli.vuejs.org/config/).
--- src/main.js
+++ src/main.js
@@ -4,7 +4,29 @@ import router from '@/router'
 
 import store from '@/state'
 
+import * as Sentry from '@sentry/browser'
+import { Vue as VueIntegration } from '@sentry/integrations'
+import { Integrations } from '@sentry/tracing'
+
 import '@/scss/app.scss'
+
+Sentry.init({
+  dsn:
+    'https://dc25995533b44e679191e7beeca10bb7@o442332.ingest.sentry.io/5427579',
+  integrations: [
+    new VueIntegration({
+      Vue,
+      tracing: true,
+      attachProps: true,
+      logErrors: true,
+      tracingOptions: {
+        trackComponents: true
+      }
+    }),
+    new Integrations.BrowserTracing()
+  ],
+  tracesSampleRate: 0.1
+})
 
 Vue.config.productionTip = false
 
